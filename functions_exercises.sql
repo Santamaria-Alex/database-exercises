c48bb3ce8ce60b78ddb84c32982b80b205ae30f9
Alex Santamaria <santamariaalex146@gmail.com>
2021-06-22 09:29:18 -0500

    completed functions ex.

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -39,8 +39,6 @@ SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'VIdya' OR f
 
 --Find all employees whose last name starts or ends with 'E' — 30,723 rows.
 SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
---Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column in your results.
-SELECT CONCAT(first_name, ' ', last_name) FROM employees WHERE last_name LIKE 'e%e';
 
 
 --Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
@@ -52,14 +50,28 @@ SELECT * FROM employees WHERE last_name LIKE 'e%e';
 SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
 
 
---Find all employees born on Christmas — 842 rows.
+
+--2. Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column in your results.
+SELECT CONCAT(first_name, ' ', last_name) FROM employees WHERE last_name LIKE 'e%e';
+
+
+--3. Find all employees born on Christmas — 842 rows.
 SELECT * FROM employees WHERE month(birth_date) = 12 AND day(birth_date) = 25;
 
 
---Find all employees hired in the 90s and born on Christmas — 362 rows.
+--4. Find all employees hired in the 90s and born on Christmas — 362 rows.
 SELECT * FROM employees WHERE (month(birth_date) = 12 AND day(birth_date) = 25) AND (year(hire_date) BETWEEN 1990 AND 1999);
 
 
---Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
-SELECT * FROM employees WHERE (month(birth_date) = 12 AND day(birth_date) = 25) AND (year(hire_date) BETWEEN 1990 AND 1999) ORDER BY ;
+--5. Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
+SELECT * FROM employees WHERE (month(birth_date) = 12 AND day(birth_date) = 25) AND (year(hire_date) BETWEEN 1990 AND 1999) ORDER BY birth_date, hire_date DESC;
+
+--6. For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You might also need to use now() or curdate()).
+SELECT *, DATEDIFF(hire_date, CURDATE()) FROM employees WHERE DAY(birth_date) = 25 AND MONTH(birth_date) = 12 AND YEAR(hire_date) LIKE '199%' ORDER BY DATEDIFF(hire_date, CURDATE()) DESC;
+
+SELECT *, DATEDIFF(hire_date, CURDATE()) FROM employees
+WHERE DAY(birth_date) = 25
+  AND MONTH(birth_date) = 12
+  AND YEAR(hire_date) LIKE '199%'
+ORDER BY DATEDIFF(hire_date, CURDATE());
 
