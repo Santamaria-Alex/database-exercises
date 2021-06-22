69f2aa0a293af36ebd8a6291e8f2c669a356558f
Alex Santamaria <santamariaalex146@gmail.com>
2021-06-22 12:08:19 -0500

    completed aliases ex.

--- sql-groupby-lec.sql
+++ sql-groupby-lec.sql
@@ -14,3 +14,10 @@ SELECT COUNT(last_name), last_name FROM employees
 WHERE last_name LIKE 'E%E'
 GROUP BY last_name
 ORDER BY last_name;
+
+
+SELECT CONCAT(first_name, ' ', last_name) AS 'Name',
+       DATEDIFF(hire_date, birth_date)/365 AS 'Age when hired'
+FROM employees
+ORDER BY DATEDIFF(hire_date, birth_date)/365 DESC
+LIMIT 10;
