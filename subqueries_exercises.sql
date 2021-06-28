e5ddf3a77b9d532b8433aff88c86469e33d9d86c
Alex Santamaria <santamariaalex146@gmail.com>
2021-06-28 10:44:01 -0500

    sub queries

---
+++ subqueries_exercises.sql
@@ -0,0 +1,58 @@
+# Find all the employees with the same hire date as employee 101010 using a subquery.
+# 69 Rows
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date IN (
+    SELECT hire_date
+    FROM employees
+    WHERE emp_no = '101010'
+);
+
+
+# Find all the titles held by all employees with the first name Aamod.
+# 314 total titles, 6 unique titles
+SELECT DISTINCT title
+FROM titles
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+);
+
+
+# Find all the current department managers that are female.
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+    WHERE to_date LIKE '9999%'
+)
+  AND gender = 'f';
+
+# Find all the department names that currently have female managers.
+SELECT dept_name
+FROM departments
+WHERE dept_no IN (
+    SELECT dept_no
+    FROM dept_manager
+    WHERE emp_no IN (
+        SELECT emp_no
+        FROM employees
+        WHERE gender = 'f'
+    )
+      AND to_date > NOW());
+
+
+# Find the first and last name of the employee with the highest salary.
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM salaries
+    WHERE salary IN (
+        SELECT MAX(salary)
+        FROM salaries
+    )
+);
+
